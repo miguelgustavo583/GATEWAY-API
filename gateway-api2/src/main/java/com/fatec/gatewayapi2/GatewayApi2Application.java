c5f0c703468263a6392c2a56280f60a5bd270675
miguelgustavo583 <miguelgscampos@gmail.com>
2026-04-25 00:37:51 -0300

    add gateway-api2

---
+++ gateway-api2/src/main/java/com/fatec/gatewayapi2/GatewayApi2Application.java
@@ -0,0 +1,13 @@
+package com.fatec.gatewayapi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApi2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApi2Application.class, args);
+    }
+
+}
---
+++ gateway-api2/src/test/java/com/fatec/gatewayapi2/GatewayApi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.gatewayapi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApi2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
